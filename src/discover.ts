5e13598a23485fa60105431902a36b7ce3a468a6
Manuel <manuel.wessner@codecentric.de>
2023-04-04 19:37:00 +0200

    WIP

--- src/discover.ts
+++ src/discover.ts
@@ -162,6 +162,7 @@ export class TestFileDiscoverer extends vscode.Disposable {
       uri.fsPath.startsWith(x),
     )
     let name
+
     if (workspacePath) {
       if (!this.workspaceCommonPrefix.has(workspacePath)) {
         const path = uri.fsPath.split(sep)
@@ -190,6 +191,7 @@ export class TestFileDiscoverer extends vscode.Disposable {
       }
 
       name = uri.fsPath.substring(workspacePrefix.length)
+      // projectId = workspacePath.split(sep).pop()
     }
     else {
       name = uri.fsPath.split(sep).pop()!
@@ -197,7 +199,14 @@ export class TestFileDiscoverer extends vscode.Disposable {
 
     const file = controller.createTestItem(uri.toString(), name, uri)
     workspacePath && this.workspaceItems.get(workspacePath)!.add(file)
-    controller.items.add(file)
+
+    const projectId = workspacePath?.split('/').pop() || 'hiho'
+    let existingItem = controller.items.get(projectId)
+    if (!existingItem)
+      existingItem = controller.createTestItem(projectId, projectId)
+
+    existingItem.children.add(file)
+    controller.items.add(existingItem)
     const data = new TestFile(file)
     WEAKMAP_TEST_DATA.set(file, data)
     this.pathToFileItem.set(uri.fsPath, data)
